d0fdb58498d06e490c4541eb5ff37b8b6e3ae3c4
mmc <llo___oll@163.com>
2020-11-20 00:08:31 +0800

    获取 推荐内容 信息，接入recyclerView

--- app/src/main/java/com/example/ximalaya/fragments/RecommendFragment.kt
+++ app/src/main/java/com/example/ximalaya/fragments/RecommendFragment.kt
@@ -9,25 +9,49 @@ import android.view.ViewGroup
 
 import com.example.ximalaya.R
 import com.example.ximalaya.base.BaseFragment
+import com.example.ximalaya.utils.Constants
+import com.example.ximalaya.utils.LogUtil
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList
 
 /**
  * A simple [Fragment] subclass.
  */
 class RecommendFragment : BaseFragment() {
+    private val TAG: String="RecommendFragment"
+
     override fun onSubViewLoaded(
         layoutInflater: LayoutInflater,
         container: ViewGroup?
     ): View {
+        //获取 猜你喜欢专辑 接口的数据
+        getRecommendData()
         return layoutInflater.inflate(R.layout.fragment_recommend,container,false)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommend, container, false)
+    /**
+     * 获取推荐内容
+     * 接口：3.10.6 获取猜你喜欢专辑
+     */
+    private fun getRecommendData() {
+        val map = hashMapOf<String,String>()
+        map.put(DTransferConstants.LIKE_COUNT,"${Constants.RECOMMEND_COUNT}")
+        CommonRequest.getGuessLikeAlbum(map,object : IDataCallBack<GussLikeAlbumList> {
+            override fun onSuccess(p0: GussLikeAlbumList?) {
+                for(i in p0!!.albumList){
+                    LogUtil.d(TAG,"${i.albumTitle} : ${i.albumIntro}")
+                }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                LogUtil.d(TAG,"error code is --> $p0,error message is --> $p1")
+            }
+
+        })
     }
+
 
 
 }
---
+++ app/src/main/java/com/example/ximalaya/utils/Constants.kt
@@ -0,0 +1,21 @@
+package com.example.ximalaya.utils
+
+/**
+ *
+ * @ProjectName:    Ximalaya
+ * @Package:        com.example.ximalaya.utils
+ * @ClassName:      Constants
+ * @Description:     常量
+ * @Author:         MMC
+ * @CreateDate:     2020/11/19 20:06
+ * @UpdateUser:     更新者
+ * @UpdateDate:     2020/11/19 20:06
+ * @UpdateRemark:   更新说明
+ * @Version:        1.0
+ */
+class Constants {
+    companion object{
+        //推荐专辑数量
+        val RECOMMEND_COUNT=20
+    }
+}
