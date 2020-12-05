e9dad77adecd4cd3210ff8c5276f19dcfcd7aa3c
mmc <llo___oll@163.com>
2020-12-05 16:22:18 +0800

    [update]完成UI回调接口

--- app/src/main/java/com/example/ximalaya/interfaces/IPlayerViewCallback.kt
+++ app/src/main/java/com/example/ximalaya/interfaces/IPlayerViewCallback.kt
@@ -1,5 +1,8 @@
 package com.example.ximalaya.interfaces
 
+import com.ximalaya.ting.android.opensdk.model.track.Track
+import com.ximalaya.ting.android.opensdk.player.service.XmPlayListControl
+
 /**
  *
  * @ProjectName:    Ximalaya
@@ -14,4 +17,58 @@ package com.example.ximalaya.interfaces
  * @Version:        1.0
  */
 interface IPlayerViewCallback {
+    /**
+     * 开始播放
+     */
+    fun onPlayStart()
+
+    /**
+     * 暂停播放
+     */
+    fun onPlayPause()
+
+    /**
+     * 停止播放
+     */
+    fun onPlayStop()
+
+    /**
+     * 播放错误
+     */
+    fun onPlayError()
+
+    /**
+     * 播放上一首
+     */
+    fun onPlayPre(track: Track)
+
+    /**
+     * 播放下一首
+     */
+    fun onPlayNext(track: Track)
+
+    /**
+     * 播放列表数据加载完成
+     */
+    fun onListLoaded(list:List<Track>)
+
+    /**
+     * 播放模式更改
+     */
+    fun onPlayModeChage(mode:XmPlayListControl.PlayMode)
+
+    /**
+     * 播放进度改变
+     */
+    fun onProgressChange(currentProgress:Long,total:Long)
+
+    /**
+     * 广告正在加载
+     */
+    fun onAdLoading()
+
+    /**
+     * 广告结束
+     */
+    fun onAdFinshed()
 }
